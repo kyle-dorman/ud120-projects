6293d7372d5625f6a7bf3ce0fb8760b8026de0e3
kyle-dorman <ked65@cornell.edu>
2016-10-23 23:44:01 -0400

    lesson 5 & 6

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import math
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -7,14 +8,25 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
 
-    
-    return cleaned_data
+    if len(predictions) != len(ages) or len(ages) != len(net_worths):
+        print "Invalid length of an input array"
+        return cleaned_data
+
+    cleaned_data = [(ages[i], net_worths[i], r_squared(predictions[i], net_worths[i])) for i in xrange(len(ages))]
+    cleaned_data = sorted(cleaned_data, key= lambda item: item[2])
+
+    last = int(math.ceil(len(cleaned_data) / 10.)) * 9
+
+    return cleaned_data[0:last]
+
+def r_squared(estimated, actual):
+    return (estimated - actual)**2
 
