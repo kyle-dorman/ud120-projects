350e06e45b13ef2a7e5816cd73553293ae08e1a6
Kyle Dorman <kdorman@gilt.com>
2016-10-25 22:26:06 -0400

    lesson 8

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -35,34 +35,30 @@ word_data = []
 ### temp_counter helps you only look at the first 200 emails in the list so you
 ### can iterate your modifications quicker
 temp_counter = 0
-specialWords = ['sara', 'shackleton', 'chris', 'germani']
-
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+#         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-            words = parseOutText(email).split()
+            words = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
-            
-            words = [word for word in words if word not in specialWords]
+            words = words.replace("sara", "").replace("shackleton", "").replace("chris", "").replace("germani", "").replace("sshacklensf", "").replace("cgermannsf", "")   
+
             ### append the text to word_data
-            word_data.append(" ".join(words))
+            word_data.append(words)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
             if name == "sara":
                 from_data.append(0)
             else:
                 from_data.append(1)
-
 
             email.close()
 
@@ -73,10 +69,13 @@ from_chris.close()
 pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
-
-
-
-
 ### in Part 4, do TfIdf vectorization here
+
+import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf.fit(word_data)
+tfidf.get_feature_names()
 
 
